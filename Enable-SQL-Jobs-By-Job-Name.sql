42e2a3d5bb1e5008799e71c205c64cf835cd4ad9
gracelugandakamya <91644009+gracelugandakamya@users.noreply.github.com>
2022-08-05 14:21:48 -0600

    enable-disable-MS-SQL-jobs

---
+++ Enable-SQL-Jobs-By-Job-Name.sql
@@ -0,0 +1,36 @@
+--Enable SQL MES Jobs By Job Name on the machine.
+USE MSDB;
+GO
+
+DECLARE @job_id uniqueidentifier
+
+DECLARE job_cursor CURSOR READ_ONLY FOR  
+SELECT job_id
+FROM msdb.dbo.sysjobs
+WHERE enabled = 0
+AND [name] like N'Sandbox%'
+
+OPEN job_cursor   
+FETCH NEXT FROM job_cursor INTO @job_id  
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+   EXEC msdb.dbo.sp_update_job @job_id = @job_id, @enabled = 1
+   FETCH NEXT FROM job_cursor INTO @job_id  
+END
+
+CLOSE job_cursor   
+DEALLOCATE job_cursor
+
+
+-- This portion of the script will return the list of the specified sql jobs so we can see if the jobs are indeed Enabled.
+SELECT 
+ job_id
+,name
+,enabled
+,date_created
+,date_modified
+FROM msdb.dbo.sysjobs
+WHERE enabled = 1
+  AND [name] like N'Sandbox%'
+ORDER BY date_created
---
+++ Powershell-script-to-enable-SQL-jobs.ps1
@@ -0,0 +1,4 @@
+$ConnectionString = 'Server=SQLSERVER01;Database=msdb;Trusted_Connection=true'
+
+Invoke-Sqlcmd -ConnectionString $ConnectionString -InputFile "C:\Temp\scripts\disable-enable-sql-jobs\Enable-SQL-Jobs-By-Job-Name.sql" | Export-Csv -Delimiter "," -Path "C:\Users\johndoe\Downloads\results.csv"
+Import-Csv "C:\Users\johndoe\Downloads\results.csv" | Format-table
